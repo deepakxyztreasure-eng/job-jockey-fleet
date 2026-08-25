b2d445cad5d136d152eaf43d780db5d7edae0f95
deepakxyz562-droid <deepakxyz562@gmail.com>
2026-08-25 14:28:31 +0530

    try to fix env issue

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "http://supabasekong-iy3enkcd5gqq1wt3ief58lfb.141.136.44.163.sslip.io";
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc4NzU5ODcyMCwiZXhwIjo0OTQzMjcyMzIwLCJyb2xlIjoiYW5vbiJ9.NCX7JgPMhfNVhWi-gvnmeZl4y0a4vNUjDzLGEDUkpGk";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "http://supabasekong-b2mjik4wa6bbiaejsfwofjbz.141.136.44.163.sslip.io";
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc4NzU4NzI2MCwiZXhwIjo0OTQzMjYwODYwLCJyb2xlIjoiYW5vbiJ9.KZsCarYscwU7CDT-LrzdXBwwJh0x509M6pIXOREFiwg";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
